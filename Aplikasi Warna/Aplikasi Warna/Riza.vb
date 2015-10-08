644c1a2faf2b1fde9a025c06fe934daa9feb385f
rizabahtiar <rizadensus87@gmail.com>
2015-10-08 13:52:51 +0700

    1431140148-Riza Bahtiar

---
+++ Aplikasi Warna/Aplikasi Warna/Riza.vb
@@ -0,0 +1,17 @@
+﻿Public Class riza
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Me.BackColor = Color.Red Then
+
+            Me.BackColor = Color.HotPink
+
+        Else
+            Me.BackColor = Color.Red
+
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.BackColor = Color.Red
+    End Sub
+End Class
